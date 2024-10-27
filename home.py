d25a7c4ab4faedae9326d24a77f03bbfdf69ec31
Meret Niklaus <niklamer@students.zhaw.ch>
2024-10-27 18:08:25 +0100

    Load the data from the csv file

---
+++ home.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+
+df_TAT = pd.read_csv ("Daten CSV.csv") # Path to the file
+print(df_TAT.head()) 
+
+
+
+
