15ec9a0712a79267f8e645d994bc8ef68a7413b8
Meret Niklaus <niklamer@students.zhaw.ch>
2024-11-06 15:10:44 +0100

    Add Colum "Zentrifugation Ende", calculate TAT of the different phases

--- home.py
+++ home.py
@@ -8,6 +8,16 @@ import streamlit as st
 df_TAT = pd.read_csv ("Daten CSV.csv") # Path to the file
 print(df_TAT.head()) 
 
+phases = {
+    "Präanalytische Phase": ["Erfassung der Anforderung", "Probenverteilung", "Proben auf Strasse","Zentrifugation"],
+    "Analytische Phase": ["Analyten werden gemessen", "Resultate werden übermittel"],
+    "Postanalytische Phase": ["Technische Validation","Medizinische Validation", "Erstellen & Versenden Bericht"]
+}
 
+# Example of accessing the data structure
+for phase, checkpoints in phases.items():
+    print(f"{phase}:")
+    for checkpoint in checkpoints:
+        print(f"  - {checkpoint}")
 
 
